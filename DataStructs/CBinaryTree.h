d2adb0a2536bd1733180b51b5402c08ca9ddb312
Rohit <rohitsinha050481@gmail.com>
2019-03-03 12:57:39 +0530

    Added File

---
+++ DataStructs/CBinaryTree.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   CBinaryTree.h
+ * Author: rohit
+ *
+ * Created on 3 March, 2019, 12:56 PM
+ */
+
+#ifndef CBINARYTREE_H
+#define CBINARYTREE_H
+
+class CBinaryTree {
+public:
+    CBinaryTree();
+    CBinaryTree(const CBinaryTree& orig);
+    virtual ~CBinaryTree();
+private:
+
+};
+
+#endif /* CBINARYTREE_H */
+
